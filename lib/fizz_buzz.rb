006de4734a61a78c57477297e7aef01d80cc1dd8
Takaki Takeuchi <takaki@takeu.ch>
2023-05-05 17:48:36 +0900

    feat(fizz_buzz): impl basic fizz_buzz logic

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,1 +1,31 @@
-puts 'Hello, World!'
+def fizz_buzz(n)
+  if n % 15 == 0
+    return 'FizzBuzz'
+  end
+
+  if n % 3 == 0
+    return 'Fizz'
+  end
+
+  if n % 5 == 0
+    return 'Buzz'
+  end
+
+  n.to_s
+end
+
+puts fizz_buzz(1)
+puts fizz_buzz(2)
+puts fizz_buzz(3)
+puts fizz_buzz(4)
+puts fizz_buzz(5)
+puts fizz_buzz(6)
+puts fizz_buzz(7)
+puts fizz_buzz(8)
+puts fizz_buzz(9)
+puts fizz_buzz(10)
+puts fizz_buzz(11)
+puts fizz_buzz(12)
+puts fizz_buzz(13)
+puts fizz_buzz(14)
+puts fizz_buzz(15)
