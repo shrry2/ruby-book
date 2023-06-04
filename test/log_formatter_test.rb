469ea61cea8ff3fd6f971ec6cb0a0a4b2a33920d
Takaki Takeuchi <takaki@takeu.ch>
2023-06-04 18:55:00 +0900

    fix(log-formatter): remove debug output

--- test/log_formatter_test.rb
+++ test/log_formatter_test.rb
@@ -4,7 +4,6 @@ require_relative '../lib/log_formatter'
 class LogFormatterTest < Minitest::Test
   def test_format_log
     text = LogFormatter.format_log
-    puts text
     lines = text.lines(chomp: true)
     assert_equal '[OK] request_id=1, path=/products/1', lines[0]
     assert_equal '[ERROR] request_id=2, path=/wp-login.php, status=404, error=Not found', lines[1]
